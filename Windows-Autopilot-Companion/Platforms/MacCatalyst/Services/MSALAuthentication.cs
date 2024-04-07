2bce18d55236acb8fdddb5c3759c311f3172e3df
brad sadd <6626255+HDBSD@users.noreply.github.com>
2024-04-08 00:28:42 +0100

    Implemented partial authenticate method

---
+++ Windows-Autopilot-Companion/Platforms/MacCatalyst/Services/MSALAuthentication.cs
@@ -0,0 +1,12 @@
+namespace Windows_Autopilot_Companion.Services
+{
+
+    public partial class MSALAuthentication
+    {
+        public partial void Authenticate(string ClientId, string[] Scopes, string RedirectURI)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
