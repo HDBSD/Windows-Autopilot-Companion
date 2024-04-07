d5f71f0313681fb71d70114527c01a408e013605
brad sadd <6626255+HDBSD@users.noreply.github.com>
2024-04-07 13:28:27 +0100

    Added MS Graph Models for Device, Organization and User

---
+++ Windows-Autopilot-Companion/Models/Device.cs
@@ -0,0 +1,18 @@
+namespace Windows_Autopilot_Companion.Models
+{
+    public class Device
+    {
+        public string? SerialNumber { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Model { get; set; }
+        public string? PurchaseOrderNumber { get; set; }
+        public string? EntraDeviceId { get; set; }
+        public string? EntraDeviceName { get; set; }
+        public string? DeploymentProfile { get; set; }
+        public string? ManagedDeviceId { get; set; }
+        public string? ManagedDeviceName { get; set; }
+        public string? GroupTag { get; set; }
+        public string? DeviceName { get; set; }
+        public string? ZtdId { get; set; }
+    }
+}
---
+++ Windows-Autopilot-Companion/Models/Org.cs
@@ -0,0 +1,8 @@
+namespace Windows_Autopilot_Companion.Models
+{
+    public class Org
+    {
+        public string? TenantID { get; set; }
+        public string? TenantDisplayName { get; set; }
+    }
+}
