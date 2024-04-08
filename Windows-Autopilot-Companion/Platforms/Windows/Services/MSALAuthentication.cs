36827a7d971ed2dce29741187a1b7fe0ea60b84f
brad sadd <6626255+HDBSD@users.noreply.github.com>
2024-04-08 01:22:51 +0100

    Implemented windows MSAL authentication class

--- Windows-Autopilot-Companion/Platforms/Windows/Services/MSALAuthentication.cs
+++ Windows-Autopilot-Companion/Platforms/Windows/Services/MSALAuthentication.cs
@@ -1,11 +1,72 @@
+using Microsoft.Identity.Client;
+
 namespace Windows_Autopilot_Companion.Services
 {
 
     public partial class MSALAuthentication
     {
-        public partial void Authenticate(string ClientId, string[] Scopes, string RedirectURI)
+        public async partial Task<AuthenticationResult> Authenticate(string ClientId, string[] Scopes, string RedirectURI)
         {
-            throw new NotImplementedException();
+
+            try
+            {
+                var app = PublicClientApplicationBuilder.Create(ClientId)
+                    .WithRedirectUri($"msal{ClientId}://auth")
+                    .WithBroker()
+                    .WithParentActivityOrWindow(GetWindowHwnd)
+                    .Build();
+
+                var accounts = await app.GetAccountsAsync();
+                var result = await app.AcquireTokenSilent(Scopes, accounts.FirstOrDefault())
+                                    .ExecuteAsync();
+
+                AuthResult = result;
+                AuthStatus = AuthenticationResultCode.Successful;
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                try
+                {
+                    var app = PublicClientApplicationBuilder.Create(ClientId)
+                        .WithRedirectUri($"msal{ClientId}://auth")
+                        .WithBroker()
+                        .WithParentActivityOrWindow(GetWindowHwnd)
+                        .Build();
+
+                    var result = await app.AcquireTokenInteractive(Scopes)
+                                        .ExecuteAsync();
+
+                    AuthResult = result;
+                    AuthStatus = AuthenticationResultCode.Successful;
+                }
+                catch (MsalException msalEx)
+                {
+                    switch (msalEx.ErrorCode)
+                    {
+                        case "authentication_canceled":
+                            AuthStatus = AuthenticationResultCode.Cancelled;
+                            break;
+                        case "access_denied":
+                            AuthStatus = AuthenticationResultCode.Denied;
+                            break;
+                        default:
+                            AuthStatus = AuthenticationResultCode.Unknown;
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                AuthStatus = AuthenticationResultCode.Unknown;
+                error = ex.Message + " " + ex.StackTrace;
+            }
+
+            return AuthResult;
+        }
+
+        public IntPtr GetWindowHwnd()
+        {
+            return ((MauiWinUIWindow)App.Current.Windows[0].Handler.PlatformView).WindowHandle;  
         }
     }
 
