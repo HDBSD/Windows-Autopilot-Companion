04ce29094253cf63adf4215173c5e7b2b2a56f80
HDBSD <6626255+HDBSD@users.noreply.github.com>
2024-04-08 12:41:03 +0100

    Fixed a typo

--- Windows-Autopilot-Companion/MainPage.xaml.cs
+++ Windows-Autopilot-Companion/MainPage.xaml.cs
@@ -15,7 +15,7 @@ namespace Windows_Autopilot_Companion
 
 			AuthenticationResult Res = await Services.MSALAuthentication.Instance.Authenticate("e432153a-3392-4262-8176-83b1fe241ad6", new string[] { "User.read.all", "Device.Read.All", "DeviceManagementConfiguration.ReadWrite.All", "DeviceManagementManagedDevices.ReadWrite.All", "DeviceManagementServiceConfig.ReadWrite.All" }, "");
 			
-			CounterBtn.Text = $"Clicked - {res.Account}";
+			CounterBtn.Text = $"Clicked - {Res.Account}";
 
 		}
 	}
