2f4b697dd72679a146ea79ae0bb3f919a49f63c3
brad sadd <6626255+HDBSD@users.noreply.github.com>
2024-04-08 01:23:13 +0100

    Hacky test of msal authentication

--- Windows-Autopilot-Companion/MainPage.xaml.cs
+++ Windows-Autopilot-Companion/MainPage.xaml.cs
@@ -9,7 +9,7 @@
 			InitializeComponent();
 		}
 
-		private void OnCounterClicked(object sender, EventArgs e)
+		private async void OnCounterClicked(object sender, EventArgs e)
 		{
 			count++;
 
@@ -19,6 +19,8 @@
 				CounterBtn.Text = $"Clicked {count} times";
 
 			SemanticScreenReader.Announce(CounterBtn.Text);
+
+			await Windows_Autopilot_Companion.Services.MSALAuthentication.Instance.Authenticate("e432153a-3392-4262-8176-83b1fe241ad6", new string[] { "User.read.all", "Device.Read.All", "DeviceManagementConfiguration.ReadWrite.All", "DeviceManagementManagedDevices.ReadWrite.All", "DeviceManagementServiceConfig.ReadWrite.All" }, "");
 		}
 	}
 
