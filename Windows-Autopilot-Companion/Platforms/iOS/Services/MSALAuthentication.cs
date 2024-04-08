0bd62f211cceac4158ed888968d590aa26ee39a6
brad sadd <6626255+HDBSD@users.noreply.github.com>
2024-04-08 20:21:29 +0100

    implemented IOS Authentication based on Android class

--- Windows-Autopilot-Companion/Platforms/iOS/Services/MSALAuthentication.cs
+++ Windows-Autopilot-Companion/Platforms/iOS/Services/MSALAuthentication.cs
@@ -7,8 +7,41 @@ namespace Windows_Autopilot_Companion.Services
 	{
 		public async partial Task<AuthenticationResult> Authenticate(string ClientId, string[] Scopes, string RedirectURI)
 		{
-			throw new NotImplementedException();
-		}
+            var app = PublicClientApplicationBuilder.Create(ClientId)
+                /* TODO -- Migrate to proper redirect URI https://learn.microsoft.com/en-us/entra/identity-platform/msal-net-use-brokers-with-xamarin-apps#step-4-add-a-redirect-uri-to-your-app-registration
+				 * "The format of the redirect URI for your application depends on the certificate used to sign the APK"
+				 * For debugging: While developing, VS signs the APK for you. Error should give the redirect URI you're meant to use, not happening in my case.
+				 */
+                .WithIosKeychainSecurityGroup("uk.co.hdbsd.wacompanion")
+                .WithRedirectUri($"msal{ClientId}://auth")
+                .Build();
+
+            var accounts = await app.GetAccountsAsync();
+            try
+            {
+                AuthResult = await app.AcquireTokenSilent(Scopes, accounts.FirstOrDefault()).ExecuteAsync();
+                AuthStatus = AuthenticationResultCode.Successful;
+            }
+            catch (MsalUiRequiredException)
+            {
+                try
+                {
+                    AuthResult = await app.AcquireTokenInteractive(Scopes).WithUseEmbeddedWebView(true).ExecuteAsync(); //uses embedded browser, attempt to skip the whole redirect URI thing. Will remove.
+                    AuthStatus = AuthenticationResultCode.Successful;
+                }
+
+                catch (MsalException ex)
+                {
+                    AuthStatus = ex.ErrorCode switch
+                    {
+                        "authentication_canceled" => AuthenticationResultCode.Cancelled,
+                        "access_denied" => AuthenticationResultCode.Denied,
+                        _ => AuthenticationResultCode.Unknown
+                    };
+                }
+            }
+            return AuthResult;
+        }
 	}
 
 }
