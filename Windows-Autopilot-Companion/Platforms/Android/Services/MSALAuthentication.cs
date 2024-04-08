14cdd16242de0dd53871d2af8abd88fba7eee487
Win7yes <7284418+francodml@users.noreply.github.com>
2024-04-07 23:41:40 -0300

    Basic MSAL implementation on Android According to docs: redirect URI will vary depending on app signature. We should probably sort out signing. Currently takes you to web page or broker, but won't sign in properly.

--- Windows-Autopilot-Companion/Platforms/Android/Services/MSALAuthentication.cs
+++ Windows-Autopilot-Companion/Platforms/Android/Services/MSALAuthentication.cs
@@ -1,4 +1,5 @@
 using Microsoft.Identity.Client;
+using Microsoft.Maui.Platform;
 
 namespace Windows_Autopilot_Companion.Services
 {
@@ -7,8 +8,44 @@ namespace Windows_Autopilot_Companion.Services
 	{
 		public async partial Task<AuthenticationResult> Authenticate(string ClientId, string[] Scopes, string RedirectURI)
 		{
-			throw new NotImplementedException();
+
+			var app = PublicClientApplicationBuilder.Create(ClientId)
+				/* TODO -- Migrate to proper redirect URI https://learn.microsoft.com/en-us/entra/identity-platform/msal-net-use-brokers-with-xamarin-apps#step-4-add-a-redirect-uri-to-your-app-registration
+				 * "The format of the redirect URI for your application depends on the certificate used to sign the APK"
+				 * For debugging: While developing, VS signs the APK for you. Error should give the redirect URI you're meant to use, not happening in my case.
+				 */
+				.WithRedirectUri($"msal{ClientId}://auth")
+				.WithBroker()
+				.WithParentActivityOrWindow(() => Platform.CurrentActivity)
+				.Build();
+
+			var accounts = await app.GetAccountsAsync();
+			try
+			{
+				AuthResult = await app.AcquireTokenSilent(Scopes, accounts.FirstOrDefault()).ExecuteAsync();
+				AuthStatus = AuthenticationResultCode.Successful;
+			}
+			catch (MsalUiRequiredException)
+			{
+				try
+				{
+					AuthResult = await app.AcquireTokenInteractive(Scopes).WithUseEmbeddedWebView(true).ExecuteAsync(); //uses embedded browser, attempt to skip the whole redirect URI thing. Will remove.
+					AuthStatus = AuthenticationResultCode.Successful;
+				}
+
+				catch (MsalException ex)
+				{
+					AuthStatus = ex.ErrorCode switch
+					{
+						"authentication_canceled" => AuthenticationResultCode.Cancelled,
+						"access_denied" => AuthenticationResultCode.Denied,
+						_ => AuthenticationResultCode.Unknown
+					};
+				}
+			}
+			return AuthResult;
 		}
+
 	}
 
 }
