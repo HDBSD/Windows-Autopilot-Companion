b1839c31ecf46c94d014aae565eec718ee825eec
brad sadd <6626255+HDBSD@users.noreply.github.com>
2024-04-10 01:04:34 +0100

    Added Account Info Page

---
+++ Windows-Autopilot-Companion/Pages/AccountInfo.xaml.cs
@@ -0,0 +1,13 @@
+using Microsoft.Identity.Client;
+
+namespace Windows_Autopilot_Companion.Pages
+{
+	public partial class AccountInfo : ContentPage
+	{
+
+		public AccountInfo()
+		{
+			InitializeComponent();
+		}
+	}
+}
