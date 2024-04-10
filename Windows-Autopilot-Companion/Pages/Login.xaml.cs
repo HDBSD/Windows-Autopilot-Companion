0ad30c7233a7b6200127d558450bfb65006daab1
brad sadd <6626255+HDBSD@users.noreply.github.com>
2024-04-10 01:04:06 +0100

    Renamed MainPage to Login

---
+++ Windows-Autopilot-Companion/Pages/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Identity.Client;
+
+namespace Windows_Autopilot_Companion.Pages
+{
+	public partial class Login : ContentPage
+	{
+
+		public Login()
+		{
+			InitializeComponent();
+		}
+
+		private async void OnLoginClicked(object sender, EventArgs e)
+		{
+
+			AuthenticationResult Res = await Services.MSALAuthentication.Instance.Authenticate("e432153a-3392-4262-8176-83b1fe241ad6", new string[] { "User.read.all", "Device.Read.All", "DeviceManagementConfiguration.ReadWrite.All", "DeviceManagementManagedDevices.ReadWrite.All", "DeviceManagementServiceConfig.ReadWrite.All" }, "");
+			
+			LoginBtn.Text = $"Clicked - {Res.Account}";
+			LoginBtn.IsVisible = false;
+			LogOffBtn.IsVisible = true;
+			Shell.Current.FlyoutBehavior = FlyoutBehavior.Flyout;
+		}
+
+		private async void OnLogoffClicked(object sender, EventArgs e)
+		{
+			LoginBtn.IsVisible = true;
+			LogOffBtn.IsVisible = false;
+			Shell.Current.FlyoutBehavior = FlyoutBehavior.Disabled;
+		}
+
+	}
+
+}
