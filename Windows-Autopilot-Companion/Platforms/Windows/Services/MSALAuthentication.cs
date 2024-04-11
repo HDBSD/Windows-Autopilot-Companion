6bff0f8ca77e7d9eabfef9962d39aa43cf0e611e
Win7yes <7284418+francodml@users.noreply.github.com>
2024-04-10 23:17:10 -0300

    Add token cache serialization Allows MSAL to save tokens so we don't have to log in manually every time.

--- Windows-Autopilot-Companion/Platforms/Windows/Services/MSALAuthentication.cs
+++ Windows-Autopilot-Companion/Platforms/Windows/Services/MSALAuthentication.cs
@@ -1,4 +1,5 @@
 using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Extensions.Msal;
 
 namespace Windows_Autopilot_Companion.Services
 {
@@ -8,13 +9,21 @@ namespace Windows_Autopilot_Companion.Services
         public async partial Task<AuthenticationResult> Authenticate(string ClientId, string[] Scopes, string RedirectURI)
         {
 
+            var storageProperties =
+                new StorageCreationPropertiesBuilder("windows_autopilot_companion_msalcache", MsalCacheHelper.UserRootDirectory)
+                .Build();
+
+            var app = PublicClientApplicationBuilder.Create(ClientId)
+                .WithRedirectUri($"msal{ClientId}://auth")
+                .WithBroker()
+                .WithParentActivityOrWindow(GetWindowHwnd)
+                .Build();
+
+            var cacheHelper = await MsalCacheHelper.CreateAsync(storageProperties);
+            cacheHelper.RegisterCache(app.UserTokenCache);
+
             try
             {
-                var app = PublicClientApplicationBuilder.Create(ClientId)
-                    .WithRedirectUri($"msal{ClientId}://auth")
-                    .WithBroker()
-                    .WithParentActivityOrWindow(GetWindowHwnd)
-                    .Build();
 
                 var accounts = await app.GetAccountsAsync();
                 var result = await app.AcquireTokenSilent(Scopes, accounts.FirstOrDefault())
@@ -27,11 +36,6 @@ namespace Windows_Autopilot_Companion.Services
             {
                 try
                 {
-                    var app = PublicClientApplicationBuilder.Create(ClientId)
-                        .WithRedirectUri($"msal{ClientId}://auth")
-                        .WithBroker()
-                        .WithParentActivityOrWindow(GetWindowHwnd)
-                        .Build();
 
                     var result = await app.AcquireTokenInteractive(Scopes)
                                         .ExecuteAsync();
