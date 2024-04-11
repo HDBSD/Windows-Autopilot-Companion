96bdbc52af7cbbc397217125fcc5b2159727b741
brad sadd <6626255+HDBSD@users.noreply.github.com>
2024-04-11 01:03:40 +0100

    Added UserProfile object to view model and async function  FetchUserProfileData to retrieve user meta data

--- Windows-Autopilot-Companion/Viewmodels/AccountInfoViewmodel.cs
+++ Windows-Autopilot-Companion/Viewmodels/AccountInfoViewmodel.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows_Autopilot_Companion.Models;
+using Windows_Autopilot_Companion.Services;
 
 namespace Windows_Autopilot_Companion.Viewmodels
 {
@@ -14,11 +16,39 @@ namespace Windows_Autopilot_Companion.Viewmodels
 		public event PropertyChangedEventHandler PropertyChanged;
 		public AuthenticationResult AuthResult { get; private set; }
 
-		public AccountInfoViewmodel()
+
+        private User _UserProfile;
+
+        public bool Loaded = false;
+
+
+		public User UserProfile 
+		{
+			get => _UserProfile;
+			set
+			{
+				if (_UserProfile != value)
+				{
+					_UserProfile = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+        public Command LoadUserData { get; set; }
+
+        public AccountInfoViewmodel()
 		{
 			AuthResult = Services.MSALAuthentication.Instance.AuthResult;
-		}
 
+            LoadUserData = new Command(async () => await FetchUserProfileData());
+        }
+
+		async Task FetchUserProfileData()
+		{
+			MSGraph_User MSGraphUserQuery = new MSGraph_User();
+            UserProfile = await MSGraphUserQuery.GetSelf();
+
+		}
 
 		public void OnPropertyChanged([CallerMemberName] string name="") =>
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
